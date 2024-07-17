9aca848d707055d447c5b96567fb0a848617457f
Armin Jodat <arminjoda@gmail.com>
2024-07-16 23:05:50 -0700

    implement dependencies for test units

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,6 +23,12 @@ dependencies {
     implementation("io.ktor:ktor-client-content-negotiation:2.3.12")
     implementation("org.slf4j:slf4j-api:1.7.1")
     implementation("org.slf4j:slf4j-simple:1.7.1")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.1")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.1")
+    testImplementation("io.mockk:mockk:1.10.6")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.4.2")
+    testImplementation("io.ktor:ktor-client-mock:1.5.4")
+    testImplementation("io.ktor:ktor-client-mock-jvm:1.5.4")
 }
 
 tasks.test {
